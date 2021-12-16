ab4e990c8c6f4d7a360755b4c67b6982b9d0c13d
Muhammad Shahzaib Ishaq <shexbi119@gmail.com>
2021-12-16 09:52:22 +0500

    testing

--- public/js/commande.js
+++ public/js/commande.js
@@ -1,7 +1,16 @@
 // Liste de tous les <select> pour les commandes
 let selects = document.querySelectorAll(".commande select");
 
-console.log("hello ");
+var commande = {};
+let zx = "hello";
+
+var es = new EventSource("/stream");
+
+es.onmessage = function(event) {
+  console.log(event.data);
+  commande = event.data;
+};
+
 /**
  * Modifie l'état d'une commande sur le serveur.
  * @param {InputEvent} event Objet d'information sur l'événement.
@@ -24,36 +33,3 @@ const modifyEtatCommande = async event => {
 for (let select of selects) {
   select.addEventListener("change", modifyEtatCommande);
 }
-
-// WS Call here!!
-
-let socket = new WebSocket("ws://localhost:5000");
-// Connection opened
-for (let i = 0; i < selects.length; i++) {
-  selects[i].addEventListener("change", function(event) {
-    socket = new WebSocket("ws://localhost:5000");
-    let data = {
-      type: "status",
-      idCommande: parseInt(
-        event.target.parentNode.parentNode.dataset.idCommande
-      ),
-      textValue: event.target.options[event.target.selectedIndex].text
-    };
-    // send Data
-    socket.addEventListener("open", function(e) {
-      socket.send(JSON.stringify(data));
-    });
-  });
-}
-
-// Listen for messages
-socket.addEventListener("message", function(e) {
-  const UpData = JSON.parse(e.data);
-  for (let select of selects) {
-    let id = parseInt(select.parentNode.parentNode.dataset.idCommande);
-    if (id == UpData.idCommande) {
-      select.options[select.selectedIndex].text = UpData.textValue;
-      break;
-    }
-  }
-});
