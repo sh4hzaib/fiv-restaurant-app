f34d6e215ccf40940be8badaf975d48bb3d856b8
Muhammad Shahzaib Ishaq <shexbi119@gmail.com>
2021-12-17 06:11:03 +0500

    if not logged in, redirect to login page

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,25 @@
-export const isAdmin = async (req,res,next)=>{
-    let isLoggedIn = req.session.isLoggedIn
-    let isLoggedUser = req.session.isLoggeduser
-        if(isLoggedIn && isLoggedUser.id_type_utilisateur ==2){
-            return next();
-        }else{
-            return res.redirect('/');
-        }
-  
-}
+export const isAdmin = async (req, res, next) => {
+  let isLoggedIn = req.session.isLoggedIn;
+  let isLoggedUser = req.session.isLoggeduser;
+  if (isLoggedIn && isLoggedUser.id_type_utilisateur == 2) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
 
-export const isAuthinticate = async (req,res,next)=>{
-    let is_log_id = req.session.isLoggedIn;
-   if(is_log_id){
-       return next()
-   }else{
-    return res.redirect('/');
-   }
-}
+export const isAuthinticate = async (req, res, next) => {
+  let is_log_id = req.session.isLoggedIn;
+  if (is_log_id) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
 
-export const isUnAuthinticate = (req,res,next)=>{
-    if(req.session.isLoggedIn){
-        return res.redirect('/');
-    }
-    return next()
-    
-}
+export const isUnAuthinticate = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    return res.redirect("/");
+  }
+  return next();
+};
